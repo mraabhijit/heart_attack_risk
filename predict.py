c1ce6c5a3149cc5c84d8ee340f57e689ff8920ca
abhijitchak103 <abhijityachak@gmail.com>
2023-10-26 20:15:10 +0530

    Flask web service of predictions

--- predict.py
+++ predict.py
@@ -1,39 +1,34 @@
 import pickle
+from flask import Flask, request, jsonify
 
 model_file = 'model.bin'
 
 with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-patient = {
-    'gender': 'female', 
-    'diabetes': 'no', 
-    'family_history': 'no', 
-    'smoking': 'no', 
-    'obesity': 'yes', 
-    'alcohol_consumption': 'no', 
-    'diet': 'average', 
-    'previous_heart_problems': 'yes', 
-    'medication_use': 'yes', 
-    'country': 'united_states', 
-    'age': 37, 
-    'cholesterol': 360, 
-    'heart_rate': 60, 
-    'exercise_hours_per_week': 17.160756256752286, 
-    'stress_level': 10, 
-    'sedentary_hours_per_day': 4.987731820348275, 
-    'income': 181290, 
-    'bmi': 35.661224922332416, 
-    'triglycerides': 369, 
-    'physical_activity_days_per_week': 5, 
-    'sleep_hours_per_day': 5, 
-    'systolic_bp': 172, 
-    'diastolic_bp': 71
-    }
+app = Flask('risk')
+
+def get_risk(patient):
+
+    X = dv.transform([patient])
+    y_pred = model.predict_proba(X)[0, 1]
+    risk = y_pred >= 0.5
+
+    return y_pred, risk
 
 
-X = dv.transform([patient])
-y_pred = model.predict_proba(X)[0, 1]
+@app.route('/predict', methods=['POST'])
+def predict_prob():
+    patient = request.get_json()
 
-print(f"Patient: \n{patient}")
-print(f"\nHeart Attack Risk Probability {round(y_pred, 3)}")
+    y_pred, risk = get_risk(patient)
+
+    result = {
+        'risk_probability': float(round(y_pred, 3)),
+        'attack risk': bool(risk)
+    }   
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
